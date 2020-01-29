a360d5d3125cbc609bfa339eaa63ba8c59d489f4
Jason Moore <jason.moore@dbca.wa.gov.au>
2020-01-29 17:33:06 +0800

    Missing Invoice Check

--- parkstay/settings.py
+++ parkstay/settings.py
@@ -1,3 +1,8 @@
+import os
+import confy
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+confy.read_environment_file(BASE_DIR+"/.env")
+os.environ.setdefault("BASE_DIR", BASE_DIR)
 from ledger.settings_base import *
 
 
