db48ce7b228e24fb090bad3064889432dc473319
mohdbakhrayba <binahmed4@gmail.com>
2020-03-09 14:44:05 +0800

    Added comment on the latest change

--- parkstay/api.py
+++ parkstay/api.py
@@ -372,7 +372,7 @@ class CampgroundMapViewSet(viewsets.ReadOnlyModelViewSet):
 
     #queryset = Campground.objects.exclude(campground_type=3).annotate(Min('campsites__rates__rate__adult'))
 
-    #
+    #Changed to speed up the loading of icons in map
     queryset = Campground.objects.exclude(campground_type=3)
 
     serializer_class = CampgroundMapSerializer
